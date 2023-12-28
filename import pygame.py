fb185f269fd3dda673def6fca65b957b4655a5c2
PanzerOne <145792717+PanzerOne@users.noreply.github.com>
2023-12-28 13:01:00 +0000

    "Few changes to the dialogue

--- import pygame.py
+++ import pygame.py
@@ -28,6 +28,21 @@ class Character:
     def boost_happiness(self):
         self.happiness += random.randint(1, 5)
 
+# define the Antagonist Class 
+class Antagonist:
+    def __init__(self,name,strength,influence):
+        self.name = name
+        self.strength = strength
+        self.influence = influence
+
+    def increase_strength(self):
+        self.strength += random.randint(1,3)
+
+    def weaken(self, amount):
+        self.strength -= amount
+        if self.strength < 0:
+            self.strength = 0
+
 # Initialize the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Cost of Justice")
@@ -62,13 +77,13 @@ while running:
 
     pygame.display.flip()
 
-    action = input("Choose an action: ")
+    action = input("You have to take a decision!! What you going to do chico?: ")
 
     if action == '1':
         new_job = input("Enter your new job: ")
         player.change_job(new_job)
         player.boost_happiness()
-        print(f"You are now a {new_job}. Your happiness increased!")
+        print(f"You are now a {new_job}. Your happiness increased!Enjoy the temporary high")
     elif action == '2':
         inflation_damage = random.randint(10, 30)
         player.happiness -= inflation_damage
@@ -78,7 +93,7 @@ while running:
         player.happiness += vacation_days * 5
         print(f"You went on vacation for {vacation_days} days and returned happier!")
     elif action == '4':
-        print("Thanks for playing!")
+        print("Thanks for playing!You Quit like everything else you do!")
         running = False
     else:
         print("Invalid choice. Please choose a valid option.")
